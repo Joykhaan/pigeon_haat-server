4a5bd9c319c64c99914d908b20ab869d15bf0380
Joykhaan <perfectkhan421@gmail.com>
2022-11-30 22:29:34 +0600

    all api done

--- index.js
+++ index.js
@@ -9,13 +9,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 app.use(cors());
 app.use(express.json());
 
-// mongodb connection
 
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfqwiph.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri)
-
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wfqwiph.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
